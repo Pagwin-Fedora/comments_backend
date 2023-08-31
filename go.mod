62f1fa1f0ffd31d821e0454b5181a8cbe833040f
Pagwin <git@pagwin.xyz>
2023-08-31 19:39:57 -0400

    began adding oauth support for a login button

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module comments_backend/m/v2
 
 go 1.21.0
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/oauth2 v0.11.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
--- init.go
+++ init.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"html/template"
-	"net/http"
-	"regexp"
-	"encoding/json"
-	_ "github.com/lib/pq"
+    "database/sql"
+    "fmt"
+    "html/template"
+    "net/http"
+    "regexp"
+    "encoding/json"
+    _ "github.com/lib/pq"
+    "golang.org/x/oauth2"
 )
 
 type Comment struct {
@@ -15,9 +16,31 @@ type Comment struct {
     Content string `json:"post"`
 }
 
+// there may be a way of using the single template in the list one but I'm not gonna bother figuring out how
+var tmpl, err = template.New("Comment template").Parse(`
+    {{define "single"}}
+	<h4> {{.Username}} posted</h4>{{.Content}}
+    {{end}}
+    {{define "list"}}
+	{{range .}}
+	    <h4> {{.Username}} posted</h4>{{.Content}}
+	{{end}}
+    {{end}}
+    `)
+
 func main(){
     connStr := "user=pqgotest dbname=pqgotest sslmode=verify-full"
     db, err := sql.Open("postgres", connStr)
+    // refer to https://github.com/golang/oauth2/blob/master/example_test.go
+    oauth_cfg := &oauth2.Config{
+		ClientID:     "YOUR_CLIENT_ID",
+		ClientSecret: "YOUR_CLIENT_SECRET",
+		Scopes:       []string{"SCOPE1", "SCOPE2"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://provider.com/o/oauth2/auth",
+			TokenURL: "https://provider.com/o/oauth2/token",
+		},
+	}
     if err != nil {
         fmt.Println(fmt.Errorf(" %w", err))
     }
@@ -35,6 +58,7 @@ return func (w http.ResponseWriter, req *http.Request){
 	return
     }
 
+    //post_fragment := post_interface("e")
     cookie, err := req.Cookie("login")
     post_fragment, err := resolve_cookie(cookie,err)
     if err != nil {
@@ -47,9 +71,11 @@ return func (w http.ResponseWriter, req *http.Request){
 
     comment := Comment{}
     json.NewDecoder(req.Body).Decode(&comment)
+
+    if comment.Content == "" {
+	return
+    }
     
-    
-    tmpl, err := template.New("Single Comment template").Parse("<h4> {{.Username}} posted</h4>{{.Content}}")
     if err != nil {
         w.Write([]byte("Template fuckup"))
         w.WriteHeader(500)
@@ -59,8 +85,7 @@ return func (w http.ResponseWriter, req *http.Request){
 
 
     //
-    tmpl.Execute(w,comment)
-    w.Write([]byte(post_fragment))
+    tmpl.ExecuteTemplate(w,"single",comment)
 }
 }
 
@@ -89,7 +114,14 @@ func resolve_cookie(cookie *http.Cookie, err error) (string, error){
 }
 
 func post_interface(username string) string{
-    return "interface to post comment goes here"
+    return `<form hx-ext="json-enc" value="submit post"id="submission-form" hx-trigger="submit" hx-target="this" hx-post="/post/" hx-swap="outerHTML">
+	<label for="username">username</label>
+	<input name="username" id="username" type="text" placeholder="username"/>
+	<label for="post">post</label>
+	<input name="post" id="post" type="text" placeholder="post"/>
+	<label for="submit">submit</label>
+	<input name="submit" id="submit" type="submit"/>
+    </form>`
 }
 
 func login_button() string{
@@ -113,25 +145,24 @@ return func(w http.ResponseWriter,req *http.Request){
     }
     w.Header().Add("Content-Type", "text/html")
 
-
-    comments := query_comments(db)
+    comments := query_comments(db, "")
     
-    cookie, err := req.Cookie("login")
-    fragment, err := resolve_cookie(cookie,err)
-    if err != nil {
-        w.Write([]byte("error handling cookie"))
-        w.WriteHeader(500)
-        return
-    }
+    //cookie, err := req.Cookie("login")
+    //fragment, err := resolve_cookie(cookie,err)
+    //if err != nil {
+    //    w.Write([]byte("error handling cookie"))
+    //    w.WriteHeader(500)
+    //    return
+    //}
+    fragment := post_interface("e")
     w.Write([]byte(fragment))
 
     //Does the work of rendering out the comments we got
-    tmpl, err := template.New("Comment list template").Parse("{{range .}} <h4> {{.Username}} posted</h4>{{.Content}}{{end}}")
     if err != nil {
         w.Write([]byte("Template fuckup"))
         w.WriteHeader(500)
         return
     }
-    tmpl.Execute(w,comments)
+    tmpl.ExecuteTemplate(w,"list",comments)
 }
 }
